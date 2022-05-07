8816439828d7d70f0295cf3af2083d4f8e7924e5
Ing. Yubisel Vega Alvarez <yubiselv@gmail.com>
2022-05-07 16:34:39 -0300

    adding hook dependencies

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -14,43 +14,30 @@ type HeaderType = {
 
 const defaultHeader: HeaderType = {
   method: "GET",
-  headers: new Headers({'content-type': 'application/json'}),
+  headers: new Headers({ 'content-type': 'application/json' }),
 }
-
-// method: 'POST', // *GET, POST, PUT, DELETE, etc.
-// mode: 'cors', // no-cors, *cors, same-origin
-// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-// credentials: 'same-origin', // include, *same-origin, omit
-// headers: {
-//   'Content-Type': 'application/json'
-//   // 'Content-Type': 'application/x-www-form-urlencoded',
-// },
-// redirect: 'follow', // manual, *follow, error
-// referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-// body: JSON.stringify(data) 
 
 function useFetch<Type>(url, header: HeaderType = defaultHeader): FetchType {
   const [data, setData] = useState<Type>();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(url, {...defaultHeader, ...header});
-      const data = await response.json();
-      setIsLoading(false);
-      setData(data);
-    } catch (error) {
-      setIsLoading(false);
-      setIsError(true);
-      console.log(error);
-    }
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, { ...defaultHeader, ...header });
+        const data = await response.json();
+        setIsLoading(false);
+        setData(data);
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error);
+      }
+    };
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return {isLoading, isError, data};
+  }, [url, header]);
+  return { isLoading, isError, data };
 };
 
 export default useFetch;
